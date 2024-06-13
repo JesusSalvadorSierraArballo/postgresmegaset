7af19dd04a2932c87f8f8af4f6b248e72f7eed2b
Jesús Salvador Sierra Arballo <jssa@citlali.mx>
2024-06-13 16:08:10 -0600

    Display node dependencies tree

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { NodeDependenciesProvider } from './nodeDependency';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -21,6 +22,11 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	// const fileProvider = new FileProvider()
+	// vscode.window.registerTreeDataProvider('nodeDependencies', fileProvider);
+	
+  	vscode.window.registerTreeDataProvider('nodeDependencies', new NodeDependenciesProvider("C:/Users/jssa/Documents/repos/example/")
+  );
 }
 
 // This method is called when your extension is deactivated
