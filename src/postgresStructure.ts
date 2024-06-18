b4c00e423fa71599ba969f8430a1affd2a2b9829
Jesús Salvador Sierra Arballo <jssa@citlali.mx>
2024-06-18 09:27:28 -0600

    Add icons to tree

--- src/postgresStructure.ts
+++ src/postgresStructure.ts
@@ -18,55 +18,39 @@ export class PostgresProvider implements vscode.TreeDataProvider<Instance> {
     return element;
   }
 
-  async getChildren(element?: Instance): Promise<Instance[]> {
+  async getChildren(element?: Instance | Database | Schema) {
 
-    if(element && element.getLevel() === DEPENDENCY_LEVEL.SCHEMAS) {
+    if(element && element instanceof Schema) {
       return Promise.resolve([
-        new Instance("Procedures", DEPENDENCY_LEVEL.OBJECTS_TYPES, vscode.TreeItemCollapsibleState.None, element.server),
-        new Instance("Tables", DEPENDENCY_LEVEL.OBJECTS_TYPES, vscode.TreeItemCollapsibleState.None, element.server)
+        new Procedure(vscode.TreeItemCollapsibleState.None, element.server),
+        new Table(vscode.TreeItemCollapsibleState.None, element.server)
       ]);
     }
-    else if(element && element.getLevel() === DEPENDENCY_LEVEL.DATABASE){
+    else if(element && element instanceof Database) {
      const schemas = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port, element.label)).getSchemas();
-     console.log(schemas);
-      let dep = schemas.rows.map((db: { nspname: string; })=> new Instance(db.nspname, DEPENDENCY_LEVEL.SCHEMAS, vscode.TreeItemCollapsibleState.Collapsed, element.server));
+      let dep = schemas.rows.map((db: { nspname: string; })=> new Schema(db.nspname, vscode.TreeItemCollapsibleState.Collapsed, element.server));
       return Promise.resolve(dep);
     }
-    else if (element && 
-    //  element.getLevel() === DEPENDENCY_LEVEL.INSTANCE
-      element instanceof Instance
-    ) {
+    else if (element && element instanceof Instance) {
       const databases = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port)).getDatabases();
       let dep = databases.rows.map((db: { datname: string; })=> 
         new Database(db.datname, 
-          DEPENDENCY_LEVEL.DATABASE, 
           vscode.TreeItemCollapsibleState.Collapsed, 
           {...element.server, database:db.datname}));
       return Promise.resolve(dep);
     } else {
       let connections =  await this.store.getConnections();
-      console.log(new Instance(connections[0].host, DEPENDENCY_LEVEL.INSTANCE, vscode.TreeItemCollapsibleState.Collapsed, connections[0]).iconPath.light);
-        const dependencies = connections.map((c) => new Instance(c.host, DEPENDENCY_LEVEL.INSTANCE, vscode.TreeItemCollapsibleState.Collapsed, c));
+      console.log(new Instance(connections[0].host, vscode.TreeItemCollapsibleState.Collapsed, connections[0]).iconPath.light);
+        const dependencies = connections.map((c) => new Instance(c.host, vscode.TreeItemCollapsibleState.Collapsed, c));
         return Promise.resolve(dependencies);
     }
   }
 }
 
-enum DEPENDENCY_LEVEL {
-  INSTANCE,
-  DATABASE,
-  SCHEMAS,
-  OBJECTS_TYPES,
-  OBJECTS,
-  TABLES,
-  PROCEDURES
-}
 
 class Instance extends vscode.TreeItem {
-  static DEPENDENCY_LEVEL = DEPENDENCY_LEVEL;
   constructor(
     public readonly label: string,
-    private dependencyLevel: DEPENDENCY_LEVEL,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public server: { user: string, password: string, host: string, port: number }
   ) {
@@ -74,21 +58,16 @@ class Instance extends vscode.TreeItem {
     this.tooltip = `${this.label}`;
 
     }
-    getLevel(): DEPENDENCY_LEVEL {
-      return this.dependencyLevel;
-    } 
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
-    dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'database.svg')
+    dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'data-cluster.svg')
   };
 }
 
 class Database extends vscode.TreeItem {
-  static DEPENDENCY_LEVEL = DEPENDENCY_LEVEL;
   constructor(
     public readonly label: string,
-    private dependencyLevel: DEPENDENCY_LEVEL,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public server: { user: string, password: string, host: string, port: number, database: string }
   ) {
@@ -96,12 +75,56 @@ class Database extends vscode.TreeItem {
     this.tooltip = `${this.label}`;
 
     }
-    getLevel(): DEPENDENCY_LEVEL {
-      return this.dependencyLevel;
-    } 
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
     dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'database.svg')
+  };
+}
+
+class Schema extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public server: { user: string, password: string, host: string, port: number, database: string }
+  ) {
+    super(label, collapsibleState);
+    this.tooltip = `${this.label}`;
+    }
+
+  iconPath = {
+    light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'schema.svg'), //TODO FIX THAT ICON COLOR
+    dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'schema.svg')
+  };
+}
+
+class Table extends vscode.TreeItem {
+  constructor(
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public server: { user: string, password: string, host: string, port: number, database: string }
+  ) {
+    super("Tables", collapsibleState);
+    this.tooltip = `${this.label}`;
+
+    }
+
+  iconPath = {
+    light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'table.svg'), //TODO FIX THAT ICON COLOR
+    dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'table.svg')
+  };
+}
+
+class Procedure extends vscode.TreeItem {
+  constructor(
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public server: { user: string, password: string, host: string, port: number, database: string }
+  ) {
+    super("Procedures", collapsibleState);
+    this.tooltip = `${this.label}`;
+    }
+
+  iconPath = {
+    light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
+    dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'procedure.svg')
   };
 }
