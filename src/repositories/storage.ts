a2e9a6b64b8733958afb9ae070bfd4198e7ff85c
Jesús Salvador Sierra Arballo <jssa@citlali.mx>
2024-06-20 10:54:57 -0600

    Chore: lint and drop useless file

--- src/repositories/storage.ts
+++ src/repositories/storage.ts
@@ -6,14 +6,14 @@ export class Storage {
     };
 
     constructor(context: ExtensionContext) { 
-        this.context = context
+        this.context = context;
      }
      private context!: ExtensionContext;
 
     async getConnections(): Promise<{user: string,password: string,host: string,port: number}[]> {
         const connections = await this.context.secrets.get(Storage.storeKeys.CONNECTIONS);
-        if(typeof connections === 'string') return JSON.parse(connections);
-        return []
+        if(typeof connections === 'string') {return JSON.parse(connections);}
+        return [];
     }
 
     async saveConection(user: string, password: string, host: string = 'localhost', port: number = 5334) {
@@ -23,14 +23,14 @@ export class Storage {
             password,
             host,
             port
-        }
+        };
         
         await this.context.secrets.store(Storage.storeKeys.CONNECTIONS, JSON.stringify([...connections, newConnection]));
     }
         
     async dropAllConnections(): Promise<{user: string,password: string,host: string,port: number}[]> {
         await this.context.secrets.store(Storage.storeKeys.CONNECTIONS, JSON.stringify([]));
-        return []
+        return [];
     }
 }
 
