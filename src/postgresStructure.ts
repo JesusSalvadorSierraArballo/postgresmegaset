e1ff49c8c0eb0d1de2316fb0fcb551a688f43dda
Jesús Salvador Sierra Arballo <jssa@citlali.mx>
2024-06-24 12:26:31 -0600

    Refactor Implement strategy to get tree childrens

--- src/postgresStructure.ts
+++ src/postgresStructure.ts
@@ -19,36 +19,8 @@ export class PostgresProvider implements vscode.TreeDataProvider<Instance> {
 
   async getChildren(element?: Instance | Database | Schema) {
 
-    if(element && element instanceof TableGroup) { 
-      const tables = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port, element.server.database))
-        .getTables(element.schema);
-      let dep = tables.rows.map((t: { table_name: string; })=> new Table(t.table_name, vscode.TreeItemCollapsibleState.None, element.server));
-      return Promise.resolve(dep);
-    }
-    else if(element && element instanceof ProcedureGroup) {
-      const procedures = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port, element.server.database))
-        .getStoreProcedure(element.schema);
-      let dep = procedures.rows.map((p: { function_name: string; })=> new Procedure(p.function_name, vscode.TreeItemCollapsibleState.Collapsed, element.server));
-      return Promise.resolve(dep);
-    }
-    else if(element && element instanceof Schema) {
-      return Promise.resolve([
-        new ProcedureGroup(vscode.TreeItemCollapsibleState.Collapsed, element.server, element.label),
-        new TableGroup(vscode.TreeItemCollapsibleState.Collapsed, element.server, element.label)
-      ]);
-    }
-    else if(element && element instanceof Database) {
-     const schemas = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port, element.label)).getSchemas();
-      let dep = schemas.rows.map((db: { schema_name: string; })=> new Schema(db.schema_name, vscode.TreeItemCollapsibleState.Collapsed, element.server));
-      return Promise.resolve(dep);
-    }
-    else if (element && element instanceof Instance) {
-      const databases = await new ConnectionInfo(new PgConnect(element.server.user, element.server.password, element.server.host, element.server.port)).getDatabases();
-      let dep = databases.rows.map((db: { datname: string; }) => 
-        new Database(db.datname, 
-          vscode.TreeItemCollapsibleState.Collapsed, 
-          {...element.server, database:db.datname}));
-      return Promise.resolve(dep);
+    if(element && element as CustomTree) { 
+      return await element.getChildrens();
     } else {
       let connections =  await this.store.getConnections();
         const dependencies = connections.map((c) => new Instance(c.host, vscode.TreeItemCollapsibleState.Collapsed, c));
@@ -65,26 +37,38 @@ export class PostgresProvider implements vscode.TreeDataProvider<Instance> {
   }
 
 }
-
-class Instance extends vscode.TreeItem {
+/////////////////////////////////////////////////////////////////
+interface CustomTree {
+  getChildrens: () => {}
+}
+class Instance extends vscode.TreeItem implements CustomTree {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public server: { user: string, password: string, host: string, port: number }
   ) {
-    super(label, collapsibleState);
-    this.tooltip = `${this.label}`;
-
+      super(label, collapsibleState);
+      this.tooltip = `${this.label}`;
     }
+
+  getChildrens = async () => {
+    const databases = await new ConnectionInfo(new PgConnect(this.server.user, this.server.password, this.server.host, this.server.port)).getDatabases();
+      let dep = databases.rows.map((db: { datname: string; }) => 
+        new Database(db.datname, 
+          vscode.TreeItemCollapsibleState.Collapsed, 
+          {...this.server, database:db.datname}));
+      return Promise.resolve(dep);
+  };
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
     dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'data-cluster.svg')
   };
+
   contextValue = 'instance';
 }
 
-export class Database extends vscode.TreeItem {
+export class Database extends vscode.TreeItem implements CustomTree {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
@@ -93,6 +77,11 @@ export class Database extends vscode.TreeItem {
     super(label, collapsibleState);
     this.tooltip = `${this.label}`;
     }
+  getChildrens = async () => {
+    const schemas = await new ConnectionInfo(new PgConnect(this.server.user, this.server.password, this.server.host, this.server.port, this.label)).getSchemas();
+      let dep = schemas.rows.map((db: { schema_name: string; })=> new Schema(db.schema_name, vscode.TreeItemCollapsibleState.Collapsed, this.server));
+      return Promise.resolve(dep);
+  };
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
@@ -102,7 +91,7 @@ export class Database extends vscode.TreeItem {
   contextValue = 'database';
 }
 
-class Schema extends vscode.TreeItem {
+class Schema extends vscode.TreeItem implements CustomTree {
   constructor(
     public readonly label: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
@@ -112,13 +101,19 @@ class Schema extends vscode.TreeItem {
     this.tooltip = `${this.label}`;
     }
 
+  getChildrens = () =>  Promise.resolve([
+      new ProcedureGroup(vscode.TreeItemCollapsibleState.Collapsed, this.server, this.label),
+      new TableGroup(vscode.TreeItemCollapsibleState.Collapsed, this.server, this.label)
+    ]);
+  
+
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'schema.svg'), //TODO FIX THAT ICON COLOR
     dark: path.join(__filename, '..','..', 'src', 'assets', 'dark', 'schema.svg')
   };
 }
 
-class TableGroup extends vscode.TreeItem {
+class TableGroup extends vscode.TreeItem implements CustomTree {
   constructor(
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public server: { user: string, password: string, host: string, port: number, database: string },
@@ -126,8 +121,14 @@ class TableGroup extends vscode.TreeItem {
   ) {
     super("Tables", collapsibleState);
     this.tooltip = `${this.label}`;
-
     }
+
+  getChildrens = async () => {
+    const tables = await new ConnectionInfo(new PgConnect(this.server.user, this.server.password, this.server.host, this.server.port, this.server.database))
+        .getTables(this.schema);
+      let dep = tables.rows.map((t: { table_name: string; })=> new Table(t.table_name, vscode.TreeItemCollapsibleState.None, this.server));
+      return Promise.resolve(dep);
+  };
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'table.svg'), //TODO FIX THAT ICON COLOR
@@ -135,7 +136,7 @@ class TableGroup extends vscode.TreeItem {
   };
 }
 
-class ProcedureGroup extends vscode.TreeItem {
+class ProcedureGroup extends vscode.TreeItem implements CustomTree {
   constructor(
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
     public server: { user: string, password: string, host: string, port: number, database: string },
@@ -144,6 +145,13 @@ class ProcedureGroup extends vscode.TreeItem {
     super("Procedures", collapsibleState);
     this.tooltip = `${this.label}`;
     }
+
+  getChildrens = async () => {
+    const procedures = await new ConnectionInfo(new PgConnect(this.server.user, this.server.password, this.server.host, this.server.port, this.server.database))
+        .getStoreProcedure(this.schema);
+      let dep = procedures.rows.map((p: { function_name: string; })=> new Procedure(p.function_name, vscode.TreeItemCollapsibleState.Collapsed, this.server));
+      return Promise.resolve(dep);
+  };
 
   iconPath = {
     light: path.join(__filename, '..','..', 'src', 'assets', 'light', 'database.svg'), //TODO FIX THAT ICON COLOR
